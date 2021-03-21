93a1001add04c416d97134da3e0d600df7b65111
wtnabe <wtnabe@gmail.com>
2021-03-21 13:36:45 +0900

    enable emptyOutdir

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import Layouts from 'vite-plugin-vue-layouts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutdir: true
+  },
   plugins: [
     vue(),
     Pages(),
