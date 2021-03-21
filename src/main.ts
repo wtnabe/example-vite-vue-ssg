b782ca49321e1a7aa5a0b9e2059a4eb481575e53
wtnabe <wtnabe@gmail.com>
2021-03-21 13:45:17 +0900

    ViteSSG を利用した createApp への置き換えのコードにすることで必要に応じて SSG を可能にする

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { ViteSSG } from 'vite-ssg'
 import { setupLayouts } from 'layouts-generated'
 import generatedRoutes from 'virtual:generated-pages'
 import App from './App.vue'
 
 const routes = setupLayouts(generatedRoutes)
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
 
-createApp(App).use(router).mount('#app')
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  ({ app, router, isClient }) => {
+  }
+)
