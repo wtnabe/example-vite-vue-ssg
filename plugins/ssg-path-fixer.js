7550d5474bbb31ca55acd4c642e9f834f66af9f4
wtnabe <wtnabe@gmail.com>
2021-03-29 09:19:35 +0900

    パスの修正時にトップの階層が二重化して出力が壊れる問題を修正

--- plugins/ssg-path-fixer.js
+++ plugins/ssg-path-fixer.js
@@ -1,7 +1,7 @@
 export function fixPathWhenSSG (route, parent) {
   if (process.env['VITE_SSG'] !== 'true') return route
 
-  if (route.component.match(/\/index\.(vue|js)$/)) {
+  if (route.component.match(/\/index\.(vue|js)$/) && route.path !== '/') {
     console.log(`\nfix index path of ${route.component} for SSG\n`)
     return {
       ...route,
